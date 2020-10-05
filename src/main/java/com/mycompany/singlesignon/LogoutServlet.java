fa27dfefd0f62ecc800c6f3ab6a091f6671a8380
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-05 21:48:16 +0800

    WebSecure

---
+++ src/main/java/com/mycompany/singlesignon/LogoutServlet.java
@@ -0,0 +1,25 @@
+package com.mycompany.singlesignon;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/singlesignon/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session == null) {
+            resp.getWriter().print("logout error");
+        } else {
+            session.invalidate();
+            resp.getWriter().print("logout OK");
+        }
+    }
+    
+}
