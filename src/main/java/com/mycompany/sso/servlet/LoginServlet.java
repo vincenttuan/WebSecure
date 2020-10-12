c7dfef3881daa8d956b436415927299fc63964e4
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-12 20:21:35 +0800

    WebSecure

--- src/main/java/com/mycompany/sso/servlet/LoginServlet.java
+++ src/main/java/com/mycompany/sso/servlet/LoginServlet.java
@@ -1,5 +1,19 @@
 package com.mycompany.sso.servlet;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/sso/login")
 public class LoginServlet extends BaseServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String grr = req.getParameter("g-recaptcha-response");
+        String json = verifyCaptcha(grr);
+        resp.getWriter().print(json);
+    }
     
 }
