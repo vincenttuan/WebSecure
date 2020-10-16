a6d74ea3a66d817caf7ac2cb2fcf5824dd223af2
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-16 21:11:53 +0800

    WebSecure

--- src/main/java/com/mycompany/sso/servlet/BaseServlet.java
+++ src/main/java/com/mycompany/sso/servlet/BaseServlet.java
@@ -8,6 +8,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Map;
@@ -20,11 +21,10 @@ public class BaseServlet extends HttpServlet {
     private static Connection conn;
     static {
         try {
-            String url = "jdbc:derby://localhost:1527/security";
-            String user = "app";
-            String password = "app";
-            conn = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/security", "app", "app");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
     
@@ -32,37 +32,32 @@ public class BaseServlet extends HttpServlet {
         int salt = new Random().nextInt(10_0000);
         password = SHA2.getSHA256(password, salt);
         int bonus = new Random().nextInt(10_0000);
-        
-        String sql_1 = "INSERT INTO Member(username, password, salt, email) VALUES ('%s', '%s', %d, '%s')";
-        String sql_2 = "INSERT INTO Salary(username, money, bonus) VALUES ('%s', %d, %d)";
-        sql_1 = String.format(sql_1, username, password, salt, email);
-        sql_2 = String.format(sql_2, username, money, bonus);
-        
-        try(Statement stmt = conn.createStatement()) {
-            conn.setAutoCommit(false); // 不要自動 commit (提交給資料庫)
+        String sql_1 = "INSERT INTO Member(username, password, salt, email) values ('" + username + "', '" + password + "', " + salt + ", '" + email + "')";
+        String sql_2 = "INSERT INTO Salary(username, money, bonus) values ('" + username + "', " + money + ", " + bonus + ")";
+        try(Statement stmt = conn.createStatement();) {
+            conn.setAutoCommit(false);
             stmt.clearBatch();
             stmt.addBatch(sql_1);
             stmt.addBatch(sql_2);
             int[] rowscount = stmt.executeBatch();
-            conn.commit(); // 手動 commit
-            
+            conn.commit();
             if(Arrays.stream(rowscount).sum() == 2) {
                 return true;
             } else {
-                conn.rollback(); // 回復原狀
+                conn.rollback();
             }
-            
-        } catch (Exception e) {
+        } catch(Exception e) {
             try {
-                conn.rollback(); // 回復原狀
-            } catch (Exception e2) {
-                e2.printStackTrace();
+                conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
+            e.printStackTrace();
         } finally {
             try {
-                conn.setAutoCommit(true); // 回復自動 commit
-            } catch (Exception e3) {
-                e3.printStackTrace();
+                conn.setAutoCommit(true);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
         }
         return false;
