9b869b2633e0e19e33eb4c82257da37694daa9f6
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-23 20:25:29 +0800

    WebSecure

--- src/main/java/com/mycompany/sso/filter/LoginFilter.java
+++ src/main/java/com/mycompany/sso/filter/LoginFilter.java
@@ -2,6 +2,7 @@ package com.mycompany.sso.filter;
 
 import java.io.IOException;
 import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
@@ -16,9 +17,13 @@ public class LoginFilter extends HttpFilter {
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpSession session = req.getSession(false);
         if(session == null || session.getAttribute("username") == null) {
-            res.sendRedirect("/WebSecure/sso/login_form.jsp");
+            //res.sendRedirect("/WebSecure/sso/login_form.jsp");
+            RequestDispatcher rd = req.getServletContext().getRequestDispatcher("/sso/login_form.jsp");
+            rd.forward(req, res);
             return;
         }
+        //String referer = req.getHeader("referer");
+        
         chain.doFilter(req, res);
     }
     
