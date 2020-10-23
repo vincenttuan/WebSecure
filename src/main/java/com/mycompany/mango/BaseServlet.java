a930029562a21f53bc0387f185833ce26ac68f3b
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-23 21:42:16 +0800

    WebSecure

--- src/main/java/com/mycompany/mango/BaseServlet.java
+++ src/main/java/com/mycompany/mango/BaseServlet.java
@@ -1,22 +1,11 @@
 package com.mycompany.mango;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.mycompany.sso.SHA2;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.Scanner;
 import javax.servlet.http.HttpServlet;
 import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.handlers.MapListHandler;
--- src/main/java/com/mycompany/mango/InputServlet.java
+++ src/main/java/com/mycompany/mango/InputServlet.java
@@ -1,6 +1,8 @@
 package com.mycompany.mango;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,7 +15,9 @@ public class InputServlet extends BaseServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Map<String, Object>> list = query();
         RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/jsp/mango/input.jsp");
+        req.setAttribute("list", list);
         rd.forward(req, resp);
     }
     
