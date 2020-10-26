664564685c9719725d9d75d59efa978c66161977
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-26 19:25:24 +0800

    WebSecure

--- src/main/java/com/mycompany/mango/BuyServlet.java
+++ src/main/java/com/mycompany/mango/BuyServlet.java
@@ -1,6 +1,7 @@
 package com.mycompany.mango;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +17,14 @@ public class BuyServlet extends BaseServlet {
         String cardNo = req.getParameter("cardNo");
         String amount = req.getParameter("amount");
         String memo = req.getParameter("memo");
-        
+        boolean check = append(myName, cardNo, Integer.parseInt(amount), memo);
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/mango/input");
+        if(check) {
+            req.setAttribute("message", "新增成功");
+        } else {
+            req.setAttribute("message", "新增失敗");
+        }
+        rd.forward(req, resp);
     }
     
 }
