6244fa91269d310ee98f50301ac36f1bb1f05abc
BD-administrator <BD-administrator@DESKTOP-FIUD33F>
2020-10-30 21:23:27 +0800

    WebSecure

--- src/main/java/com/mycompany/mango/BuyServlet.java
+++ src/main/java/com/mycompany/mango/BuyServlet.java
@@ -1,6 +1,8 @@
 package com.mycompany.mango;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,14 +10,17 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/mango/buy")
 public class BuyServlet extends BaseServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String myName = req.getParameter("myName");
-        String cardNo = req.getParameter("cardNo");
+        HttpSession session = req.getSession(false);
+        List<Map<String, Object>> member = (List<Map<String, Object>>)session.getAttribute("member");
+        String myName = member.get(0).get("USERNAME") + "";
+        String cardNo = member.get(0).get("CARDNO") + "";
         String amount = req.getParameter("amount");
         String memo = req.getParameter("memo");
         boolean check = append(myName, cardNo, Integer.parseInt(amount), memo);
